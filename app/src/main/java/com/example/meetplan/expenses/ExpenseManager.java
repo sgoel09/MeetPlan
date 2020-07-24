488f1e6f4a836f09e37b513bf2f3141c61b8ec1b
Shefali Goel <shgoel@fb.com>
2020-07-24 12:18:36 -0700

    Completed algorithm to compute even split transactions after retrieving data from database

--- app/src/main/java/com/example/meetplan/expenses/ExpenseManager.java
+++ app/src/main/java/com/example/meetplan/expenses/ExpenseManager.java
@@ -21,12 +21,17 @@ import static java.lang.Math.abs;
 
 public class ExpenseManager {
 
-    private static final String KEY_MEETUP = "meetup";
-    private static final String TAG = "ExpenseCalculator";
+    private static final String KEY_OBJECT_ID = "objectId";
+    private static final String KEY_SPLIT_EXPENSE = "splitexpense";
+    private static final String KEY_EXPENSES = "expenses";
+    private static final double ROUND_DOUBLE = 100.0;
+    private static final int UNIQUE_LIMIT = 1;
+    private static final String TAG = "ExpenseManager";
     private Meetup meetup;
     private ImmutableList<Expense> allExpenses;
     private HashMap<String, Double> allNets;
     private ImmutableSortedMap<String, Double> allNetsImmutable;
+    private int numQueried = 0;
 
     public ExpenseManager(Meetup meetup) {
         this.meetup = meetup;
@@ -34,7 +39,6 @@ public class ExpenseManager {
     }
 
     public void calculateTransactions() {
-        calculateNetTotals();
         while (allNets.size() > 1) {
             makeTransaction();
         }
@@ -59,7 +63,7 @@ public class ExpenseManager {
             int share = entry.getValue();
             double personalTotal = 0;
             if (user.equals(paid)) {
-                personalTotal = amount;
+                personalTotal = amount - calculatePersonalTotal(share, total, amount);
             } else {
                 personalTotal = -calculatePersonalTotal(share, total, amount);
             }
@@ -68,6 +72,9 @@ public class ExpenseManager {
             } else {
                 allNets.put(user, personalTotal);
             }
+        }
+        if (numQueried == allExpenses.size()) {
+            calculateTransactions();
         }
     }
 
@@ -90,26 +97,27 @@ public class ExpenseManager {
             allNets.remove(creditor);
         }
         sortMap();
+        transactionAmount = Math.round(transactionAmount * ROUND_DOUBLE) / ROUND_DOUBLE;
         Log.i(TAG, debtor + " pays " + creditor + " $" + transactionAmount);
     }
 
     private void sortMap() {
-        Ordering naturalOrdering = Ordering.natural().onResultOf(Functions.forMap(allNets, null));
+        Ordering naturalOrdering = Ordering.natural().onResultOf(Functions.forMap(allNets, null)).compound(Ordering.arbitrary());
         allNetsImmutable = ImmutableSortedMap.copyOf(allNets, naturalOrdering);
     }
 
     private void queryExpenses(final Meetup meetup) {
         final List<Expense>[] expenses = new List[]{null};
         ParseQuery<Meetup> query = ParseQuery.getQuery(Meetup.class);
-        query.whereEqualTo("objectId", meetup.getObjectId());
-        query.setLimit(1);
-        query.include("expenses");
+        query.whereEqualTo(KEY_OBJECT_ID, meetup.getObjectId());
+        query.setLimit(UNIQUE_LIMIT);
+        query.include(KEY_EXPENSES);
         query.findInBackground(new FindCallback<Meetup>() {
             @Override
             public void done(List<Meetup> meetups, ParseException e) {
                 expenses[0] = meetups.get(0).getExpenses();
                 allExpenses = ImmutableList.<Expense>builder().addAll(expenses[0]).build();
-                calculateTransactions();
+                calculateNetTotals();
             }
         });
     }
@@ -117,14 +125,14 @@ public class ExpenseManager {
     private void querySplitExpense(final Expense expense) {
         final SplitExpense[] newExpence = {new SplitExpense()};
         ParseQuery<Expense> query = ParseQuery.getQuery(Expense.class);
-        query.whereEqualTo("objectId", expense.getObjectId());
-        query.setLimit(1);
-        query.include("splitexpense");
+        query.whereEqualTo(KEY_OBJECT_ID, expense.getObjectId());
+        query.setLimit(UNIQUE_LIMIT);
+        query.include(KEY_SPLIT_EXPENSE);
         query.findInBackground(new FindCallback<Expense>() {
             @Override
             public void done(List<Expense> expenses, ParseException e) {
                 newExpence[0] = expenses.get(0).getSplitExpense();
-                Log.i("QuerySplitExpense", newExpence[0].toString());
+                numQueried += 1;
                 eachExpense(expense, newExpence[0]);
             }
         });
