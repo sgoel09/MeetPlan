7833e0b713037b02d0d87011eb2200151084efb8
Shefali Goel <shgoel@fb.com>
2020-07-31 11:41:00 -0700

    Created layouts for gallery fragment and item picture

---
+++ app/src/main/java/com/example/meetplan/gallery/GalleryAdapter.java
@@ -0,0 +1,4 @@
+package com.example.meetplan.gallery;
+
+public class GalleryAdapter {
+}
---
+++ app/src/main/java/com/example/meetplan/gallery/GalleryFragment.java
@@ -0,0 +1,42 @@
+package com.example.meetplan.gallery;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.meetplan.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GalleryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GalleryFragment extends Fragment {
+
+    public GalleryFragment() {
+        // Required empty public constructor
+    }
+
+    public static GalleryFragment newInstance(String param1, String param2) {
+        GalleryFragment fragment = new GalleryFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+}
