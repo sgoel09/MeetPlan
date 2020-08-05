e09495cfa412dffa76315bd6dd6ec1f109abd578
Shefali Goel <shgoel@fb.com>
2020-08-05 11:06:55 -0700

    Added share intent to share an image in the gallery

--- app/src/main/java/com/example/meetplan/gallery/ImageFragment.java
+++ app/src/main/java/com/example/meetplan/gallery/ImageFragment.java
@@ -1,14 +1,20 @@
 package com.example.meetplan.gallery;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.ShareActionProvider;
+import androidx.core.content.FileProvider;
 import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -21,15 +27,22 @@ import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.example.meetplan.MainActivity;
 import com.example.meetplan.R;
 import com.example.meetplan.databinding.FragmentGalleryBinding;
 import com.example.meetplan.databinding.FragmentImageBinding;
 import com.parse.ParseFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ImageFragment extends Fragment {
 
     private static final String KEY_IMAGE_RES = "image";
     private FragmentImageBinding binding;
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
 
     public ImageFragment() {
         // Required empty public constructor
@@ -81,10 +94,48 @@ public class ImageFragment extends Fragment {
                         // startPostponedEnterTransition() should also be called on it to get the transition
                         // going when the image is ready.
                         getParentFragment().startPostponedEnterTransition();
+                        prepareShareIntent(((BitmapDrawable) resource).getBitmap());
+                        attachShareIntentAction();
                         return false;
                     }
                 })
                 .into(binding.imageView);
+    }
+
+    private void attachShareIntentAction() {
+        if (miShareAction != null && shareIntent != null)
+            miShareAction.setShareIntent(shareIntent);
+    }
+
+    private void prepareShareIntent(Bitmap bitmap) {
+        Uri bmpUri = getBitmapFromDrawable(bitmap);// see previous remote images section and notes for API > 23
+        // Construct share intent as described above based on bitmap
+        shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+    }
+
+    private Uri getBitmapFromDrawable(Bitmap bmp) {
+        Uri bmpUri = null;
+        try {
+            // Use methods on Context to access package-specific directories on external storage.
+            // This way, you don't need to request external read/write permission.
+            // See https://youtu.be/5xVh-7ywKpE?t=25m25s
+            File file =  new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+
+            // wrap File object into a content provider. NOTE: authority here should match authority in manifest declaration
+            bmpUri = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovidermeetplan", file);  // use this version for API >= 24
+
+            // **Note:** For API < 24, you may use bmpUri = Uri.fromFile(file);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 
     @Override
@@ -94,6 +145,7 @@ public class ImageFragment extends Fragment {
         // Locate MenuItem with ShareActionProvider
         MenuItem item = menu.findItem(R.id.menu_item_share);
         // Fetch reference to the share action provider
-        ShareActionProvider miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        attachShareIntentAction();
     }
 }
